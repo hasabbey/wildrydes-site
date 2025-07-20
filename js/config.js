0d38e2e89ad5aa87d473a5fb30d7a0eb1bb170ef
hasabbey <162728232+hasabbey@users.noreply.github.com>
2025-07-20 13:50:51 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,9 +1,9 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_xU7iFEdbv
-', // e.g. us-east-2_uXboG5pAb
+        userPoolId: 'us-east-1_xU7iFEdbv',
+ // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '78n04nsebjk07k7rvmi07ck4oa', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
